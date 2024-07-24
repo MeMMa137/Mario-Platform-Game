1277d35fe3e40bcbf5e4edbb5933b2125bed7647
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-24 23:57:07 +0200

    In-game items added

--- MarioArcadeGame/app/src/main/java/components/Fireball.java
+++ MarioArcadeGame/app/src/main/java/components/Fireball.java
@@ -9,7 +9,6 @@ import physics2d.Physics2D;
 import physics2d.components.Rigidbody2D;
 
 public class Fireball extends Component {
-
     public transient boolean goingRight = false;
     private transient Rigidbody2D rb;
     private transient float fireballSpeed = 1.7f;
@@ -53,8 +52,35 @@ public class Fireball extends Component {
         } else {
             this.acceleration.y = Window.getPhysics().getGravity().y * 0.7f;
         }
+
         this.velocity.y += this.acceleration.y * dt;
         this.velocity.y = Math.max(Math.min(this.velocity.y, this.terminalVelocity.y), -terminalVelocity.y);
         this.rb.setVelocity(velocity);
-    
+    }
+
+    public void checkOnGround() {
+        float innerPlayerWidth = 0.25f * 0.7f;
+        float yVal = -0.09f;
+        onGround = Physics2D.checkOnGround(this.gameObject, innerPlayerWidth, yVal);
+    }
+
+    @Override
+    public void beginCollision(GameObject obj, Contact contact, Vector2f contactNormal) {
+        if (Math.abs(contactNormal.x) > 0.8f) {
+            this.goingRight = contactNormal.x < 0;
+        }
+    }
+
+    @Override
+    public void preSolve(GameObject obj, Contact contact, Vector2f contactNormal) {
+        if (obj.getComponent(PlayerController.class) != null ||
+            obj.getComponent(Fireball.class) != null) {
+            contact.setEnabled(false);
+        }
+    }
+
+    public void disappear() {
+        fireballCount--;
+        this.gameObject.destroy();
+    }
 }
