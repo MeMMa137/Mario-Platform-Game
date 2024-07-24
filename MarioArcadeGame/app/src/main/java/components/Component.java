2dc5a2bee6461fded2664d234311f9f9fa2d414e
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-24 16:22:25 +0200

    Introduction of coins

---
+++ MarioArcadeGame/app/src/main/java/components/Component.java
@@ -0,0 +1,63 @@
+package components;
+
+import editor.JImGui;
+import imgui.ImGui;
+import imgui.type.ImInt;
+import jade.GameObject;
+import org.jbox2d.dynamics.contacts.Contact;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public abstract class Component {
+    private static int ID_COUNTER = 0;
+    private int uid = -1;
+
+    public transient GameObject gameObject = null;
+
+    public void start() {
+
+    }
+
+    public void update(float dt) {
+
+    }
+
+    public void editorUpdate(float dt) {
+
+    }
+
+    public void beginCollision(GameObject collidingObject, Contact contact, Vector2f hitNormal) {
+
+    }
+
+    public void endCollision(GameObject collidingObject, Contact contact, Vector2f hitNormal) {
+
+    }
+
+    public void preSolve(GameObject collidingObject, Contact contact, Vector2f hitNormal) {
+
+    }
+
+    public void postSolve(GameObject collidingObject, Contact contact, Vector2f hitNormal) {
+
+    }
+    public void generateId() {
+        if (this.uid == -1) {
+            this.uid = ID_COUNTER++;
+        }
+    }
+
+    private <T extends Enum<T>> String[] getEnumValues(Class<T> enumType) {
+        String[] enumValues = new String[enumType.getEnumConstants().length];
+        int i = 0;
+        for (T enumIntegerValue : enumType.getEnumConstants()) {
+            enumValues[i] = enumIntegerValue.name();
+            i++;
+        }
+        return enumValues;
+    }
+}
