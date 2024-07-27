1f94dc1c1f63dbbc4b41c88ba4e04c8fb33156f6
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-27 11:59:06 +0200

    Player controls

--- MarioArcadeGame/app/src/main/java/components/GoombaAI.java
+++ MarioArcadeGame/app/src/main/java/components/GoombaAI.java
@@ -28,3 +28,82 @@ public class GoombaAI extends Component {
         this.rb = gameObject.getComponent(Rigidbody2D.class);
         this.acceleration.y = Window.getPhysics().getGravity().y * 0.7f;
     }
+
+@Override
+    public void update(float dt) {
+        Camera camera = Window.getScene().camera();
+        if (this.gameObject.transform.position.x >
+                camera.position.x + camera.getProjectionSize().x * camera.getZoom()) {
+            return;
+        }
+
+        if (isDead) {
+            timeToKill -= dt;
+            if (timeToKill <= 0) {
+                this.gameObject.destroy();
+            }
+            this.rb.setVelocity(new Vector2f());
+            return;
+        }
+
+        if (goingRight) {
+            velocity.x = walkSpeed;
+        } else {
+            velocity.x = -walkSpeed;
+        }
+
+        checkOnGround();
+        if (onGround) {
+            this.acceleration.y = 0;
+            this.velocity.y = 0;
+        } else {
+            this.acceleration.y = Window.getPhysics().getGravity().y * 0.7f;
+        }
+
+        this.velocity.y += this.acceleration.y * dt;
+        this.velocity.y = Math.max(Math.min(this.velocity.y, this.terminalVelocity.y), -terminalVelocity.y);
+        this.rb.setVelocity(velocity);
+        if (this.gameObject.transform.position.x < Window.getScene().camera().position.x - 0.5f) {
+            this.gameObject.destroy();
+        }
+    }
+
+    public void checkOnGround() {
+        float innerPlayerWidth = 0.25f * 0.7f;
+        float yVal = -0.14f;
+        onGround = Physics2D.checkOnGround(this.gameObject, innerPlayerWidth, yVal);
+    }
+
+    @Override
+    public void preSolve(GameObject obj, Contact contact, Vector2f contactNormal) {
+        if (isDead) {
+            return;
+        }
+
+        PlayerController playerController = obj.getComponent(PlayerController.class);
+        if (playerController != null) {
+            if (!playerController.isDead() && !playerController.isHurtInvincible() &&
+                    contactNormal.y > 0.58f) {
+                playerController.enemyBounce();
+                stomp();
+            } else if (!playerController.isDead() && !playerController.isInvincible()) {
+                playerController.die();
+                if (!playerController.isDead()) {
+                    contact.setEnabled(false);
+                }
+            } else if (!playerController.isDead() && playerController.isInvincible()) {
+                contact.setEnabled(false);
+            }
+        } else if (Math.abs(contactNormal.y) < 0.1f) {
+            goingRight = contactNormal.x < 0;
+        }
+
+        if (obj.getComponent(Fireball.class) != null) {
+            stomp();
+            obj.getComponent(Fireball.class).disappear();
+        }
+    }
+
+    public void stomp() {
+        stomp(true);
+    }
