a237aecf306a722255c08d5ae73e38d01bb48497
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-30 00:27:27 +0200

    Debug Draw

---
+++ MarioArcadeGame/app/src/main/java/renderer/DebugDraw.java
@@ -0,0 +1,75 @@
+package renderer;
+
+import jade.Camera;
+import jade.Window;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import util.AssetPool;
+import util.JMath;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+public class DebugDraw {
+    private static int MAX_LINES = 5000;
+
+    private static List<Line2D> lines = new ArrayList<>();
+    // 6 floats per vertex, 2 vertices per line
+    private static float[] vertexArray = new float[MAX_LINES * 6 * 2];
+    private static Shader shader = AssetPool.getShader("assets/shaders/debugLine2D.glsl");
+
+    private static int vaoID;
+    private static int vboID;
+
+    private static boolean started = false;
+
+    public static void start() {
+        // Generate the vao
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        // Create the vbo and buffer some memory
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexArray.length * Float.BYTES, GL_DYNAMIC_DRAW);
+
+        // Enable the vertex array attributes
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
+        glEnableVertexAttribArray(1);
+
+        glLineWidth(2.0f);
+    }
+
+    public static void beginFrame() {
+        if (!started) {
+            start();
+            started = true;
+        }
+
+        // Remove dead lines
+        for (int i=0; i < lines.size(); i++) {
+            if (lines.get(i).beginFrame() < 0) {
+                lines.remove(i);
+                i--;
+            }
+        }
+    }
+    
+
+ public static void addLine2D(Vector2f from, Vector2f to) {
+        // TODO: ADD CONSTANTS FOR COMMON COLORS
+        addLine2D(from, to, new Vector3f(0, 1, 0), 1);
+    }
+
+    public static void addLine2D(Vector2f from, Vector2f to, Vector3f color) {
+        addLine2D(from, to, color, 1);
+    }
