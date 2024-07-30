6b29c4839953624d5f523492b99deebbc4300a6e
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-30 08:07:25 +0200

    Drawing of shapes

--- MarioArcadeGame/app/src/main/java/renderer/DebugDraw.java
+++ MarioArcadeGame/app/src/main/java/renderer/DebugDraw.java
@@ -73,3 +73,136 @@ public class DebugDraw {
     public static void addLine2D(Vector2f from, Vector2f to, Vector3f color) {
         addLine2D(from, to, color, 1);
     }
+    
+public static void draw() {
+        if (lines.size() <= 0) return;
+
+        int index = 0;
+        for (Line2D line : lines) {
+            for (int i=0; i < 2; i++) {
+                Vector2f position = i == 0 ? line.getFrom() : line.getTo();
+                Vector3f color = line.getColor();
+
+                // Load position
+                vertexArray[index] = position.x;
+                vertexArray[index + 1] = position.y;
+                vertexArray[index + 2] = -10.0f;
+
+                // Load the color
+                vertexArray[index + 3] = color.x;
+                vertexArray[index + 4] = color.y;
+                vertexArray[index + 5] = color.z;
+                index += 6;
+            }
+        }
+
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexArray, GL_DYNAMIC_DRAW);
+
+        // Use our shader
+        shader.use();
+        shader.uploadMat4f("uProjection", Window.getScene().camera().getProjectionMatrix());
+        shader.uploadMat4f("uView", Window.getScene().camera().getViewMatrix());
+
+        // Bind the vao
+        glBindVertexArray(vaoID);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        // Draw the batch
+        glDrawArrays(GL_LINES, 0, lines.size());
+
+        // Disable Location
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+
+        // Unbind shader
+        shader.detach();
+    }
+
+    // ==================================================
+    // Add line2D methods
+    // ==================================================
+
+    public static void addLine2D(Vector2f from, Vector2f to, Vector3f color, int lifetime) {
+        Camera camera = Window.getScene().camera();
+        Vector2f cameraLeft = new Vector2f(camera.position).add(new Vector2f(-2.0f, -2.0f));
+        Vector2f cameraRight = new Vector2f(camera.position).
+                add(new Vector2f(camera.getProjectionSize()).mul(camera.getZoom())).
+                add(new Vector2f(4.0f, 4.0f));
+        boolean lineInView =
+                ((from.x >= cameraLeft.x && from.x <= cameraRight.x) && (from.y >= cameraLeft.y && from.y <= cameraRight.y)) ||
+                ((to.x >= cameraLeft.x && to.x <= cameraRight.x) && (to.y >= cameraLeft.y && to.y <= cameraRight.y));
+        if (lines.size() >= MAX_LINES || !lineInView) {
+            return;
+        }
+        DebugDraw.lines.add(new Line2D(from, to, color, lifetime));
+    }
+
+    // ==================================================
+    // Add Box2D methods
+    // ==================================================
+    public static void addBox2D(Vector2f center, Vector2f dimensions, float rotation) {
+        // TODO: ADD CONSTANTS FOR COMMON COLORS
+        addBox2D(center, dimensions, rotation, new Vector3f(0, 1, 0), 1);
+    }
+
+    public static void addBox2D(Vector2f center, Vector2f dimensions, float rotation, Vector3f color) {
+        addBox2D(center, dimensions, rotation, color, 1);
+    }
+
+    public static void addBox2D(Vector2f center, Vector2f dimensions, float rotation,
+                                Vector3f color, int lifetime) {
+        Vector2f min = new Vector2f(center).sub(new Vector2f(dimensions).mul(0.5f));
+        Vector2f max = new Vector2f(center).add(new Vector2f(dimensions).mul(0.5f));
+
+        Vector2f[] vertices = {
+              new Vector2f(min.x, min.y), new Vector2f(min.x, max.y),
+              new Vector2f(max.x, max.y), new Vector2f(max.x, min.y)
+        };
+
+        if (rotation != 0.0f) {
+            for (Vector2f vert : vertices) {
+                JMath.rotate(vert, rotation, center);
+            }
+        }
+
+        addLine2D(vertices[0], vertices[1], color, lifetime);
+        addLine2D(vertices[0], vertices[3], color, lifetime);
+        addLine2D(vertices[1], vertices[2], color, lifetime);
+        addLine2D(vertices[2], vertices[3], color, lifetime);
+    }
+
+    // ==================================================
+    // Add Circle methods
+    // ==================================================
+    public static void addCircle(Vector2f center, float radius) {
+        // TODO: ADD CONSTANTS FOR COMMON COLORS
+        addCircle(center, radius, new Vector3f(0, 1, 0), 1);
+    }
+
+    public static void addCircle(Vector2f center, float radius, Vector3f color) {
+        addCircle(center, radius, color, 1);
+    }
+
+    public static void addCircle(Vector2f center, float radius, Vector3f color, int lifetime) {
+        Vector2f[] points = new Vector2f[20];
+        int increment = 360 / points.length;
+        int currentAngle = 0;
+
+        for (int i=0; i < points.length; i++) {
+            Vector2f tmp = new Vector2f(0, radius);
+            JMath.rotate(tmp, currentAngle, new Vector2f());
+            points[i] = new Vector2f(tmp).add(center);
+
+            if (i > 0) {
+                addLine2D(points[i - 1], points[i], color, lifetime);
+            }
+            currentAngle += increment;
+        }
+
+        addLine2D(points[points.length - 1], points[0], color, lifetime);
+    }
+}
+
